d253c79fa3393a9875ebb5556a94473f89905e26
DeclanWu <qianshuiqianshui@gmail.com>
2017-11-21 14:22:15 +0800

    Initial commit

---
+++ app/src/main/java/com/jusisoft/iddzb/LauncherActivity.java
@@ -0,0 +1,20 @@
+package com.jusisoft.iddzb;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by wuzhouxing on 17-11-14.
+ */
+
+public class LauncherActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
---
+++ app/src/main/jni/launcheractivity.c
@@ -0,0 +1,8 @@
+#include <jni.h>
+
+JNIEXPORT void JNICALL
+Java_com_jusisoft_iddzb_LauncherActivity_Hello(JNIEnv *env, jclass type) {
+
+    // TODO
+
+}
